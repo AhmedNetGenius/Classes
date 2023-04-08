1ebadf88de490d548cc23faf36e201c336bf6cda
HassanDurra <Hassan2109f@aptechgdn.net>
2023-04-08 19:13:42 +0100

    form validation completed

--- assets/js/custom/webcam.js
+++ assets/js/custom/webcam.js
@@ -12,11 +12,27 @@ function opencam()
         Png_quality : 90
     });
     Webcam.attach(videoSection);
+    videoSection.style.display = 'block' ;
+    results.style.display = 'none' ;    
+    download.style.display = 'none' ;
 }
 
 function takeSnap()
 {
     Webcam.snap(function(e){
-        
+        results.innerHTML = "<img src = "+e+">" ;
+        videoSection.style.display = 'none' ;
+        download.innerHTML = "<a href = "+e+" download >Download Image</a>";
+        results.style.display = 'block' ;    
+        download.style.display = 'block' ;
+        Webcam.reset(); 
     })
+}
+
+function camclose()
+{
+    Webcam.reset();
+    videoSection.style.display = 'none' ;
+    results.style.display = 'none' ;    
+    download.style.display = 'none' ;
 }
